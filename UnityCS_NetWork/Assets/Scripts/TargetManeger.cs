a75f640efba9f068b93269ad8567ccac1b2a3de1
TagamiMotohiro <105412324+TagamiMotohiro@users.noreply.github.com>
2022-12-17 17:03:28 +0900

    田上　ターゲットランダム生成の基盤を作成しました(まだ生成はできない)

--- UnityCS_NetWork/Assets/Scripts/TargetManeger.cs
+++ UnityCS_NetWork/Assets/Scripts/TargetManeger.cs
@@ -1,19 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class TargetManeger : MonoBehaviour
+using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+public class TargetManeger : MonoBehaviourPunCallbacks
 {
+    [SerializeField]
+    int spawn_Limit = 10;//ターゲットの沸き数上限
 
+    //ゲームオブジェクトのTransFormを指定してターゲットが沸く範囲を指定
+    [SerializeField]
+    Transform rightUp_Point;
+    [SerializeField]
+    Transform leftUp_Point;
+    [SerializeField]
+    Transform rightDown_Point;
+    [SerializeField]
+    Transform leftDown_Point;
+    [SerializeField]
+    Transform Depth_Point;
+
+    List<GameObject> targetList;//ターゲット沸き数管理用のList
+
+    //沸き位置指定用のランダム上限下限
+    float random_x_min=0;
+    float random_x_max=0;
+    float random_y_min=0;
+    float random_y_max=0;
+    float random_z_min=0;
+    float random_z_max=0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        TargetInit();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (PhotonNetwork.LocalPlayer.IsMasterClient)
+        {
+            float random_x = Random.Range(random_x_min,random_x_max);
+            float random_y = Random.Range(random_y_min,random_y_max);
+            float random_z = Random.Range(random_z_min,random_z_max);
+        }
+    }
+
+    void TargetInit()
+    { 
+    random_x_min = leftUp_Point.position.x;
+    random_x_max = rightUp_Point.position.x;
+    random_y_min = leftDown_Point.position.y;
+    random_y_max = leftUp_Point.position.y;
+    random_z_min = leftUp_Point.position.z;
+    random_z_max = Depth_Point.position.z;
     }
 }
