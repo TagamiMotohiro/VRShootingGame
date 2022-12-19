4c916b39e8802c1abc771c78a8d3a42b1a523f4e
TagamiMotohiro <105412324+TagamiMotohiro@users.noreply.github.com>
2022-12-19 15:08:17 +0900

    Revert "田上　的生成が変だったので修正　一部担当箇所にコメントを追加"
    This reverts commit 64e572d74df1d709cc7889b435808c517b1cab76.

--- UnityCS_NetWork/Assets/Scripts/BulletCtrl.cs
+++ UnityCS_NetWork/Assets/Scripts/BulletCtrl.cs
@@ -7,15 +7,12 @@ public class BulletCtrl : MonoBehaviourPunCallbacks
 {
     [SerializeField]
     GameObject HitEffect;
-    [SerializeField]
-    float dulation;//弾の生存時間
     float time=0;
     // Start is called before the first frame update
     void Start()
     {
         if (!photonView.IsMine)
         {
-            //放たれた弾が自身のものでなければレイヤーを敵弾用のものに変更
             this.gameObject.layer = 10;
         }
     }
@@ -24,9 +21,8 @@ public class BulletCtrl : MonoBehaviourPunCallbacks
     void Update()
     {
         time += Time.deltaTime;
-        if (time >= dulation)
+        if (time >= 3)
         {
-            //自身の放った球は一定秒数でヒットしなくても消滅
             if (!photonView.IsMine) { return; }
             PhotonNetwork.Destroy(this.gameObject);
         }
@@ -34,11 +30,13 @@ public class BulletCtrl : MonoBehaviourPunCallbacks
 	private void OnDestroy()
 	{
 		Instantiate(HitEffect,transform.position,Quaternion.identity);
-        //消滅時エフェクト
 	}
 	private void OnCollisionEnter(Collision collision)
     {
         Destroy(gameObject);
-        //ダメージ判定は的側なので弾側の処理は消すだけ
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.SendMessage("damege");
+        }
     }
 }
