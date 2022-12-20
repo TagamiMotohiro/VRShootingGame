32bb4cef4315ed5e37a698e38aa96fcf25c3f173
TagamiMotohiro <105412324+TagamiMotohiro@users.noreply.github.com>
2022-12-20 17:28:41 +0900

    時間表示に対応

--- UnityCS_NetWork/Assets/Scripts/TimeManeger.cs
+++ UnityCS_NetWork/Assets/Scripts/TimeManeger.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class TimeManeger : MonoBehaviour
+using Photon.Realtime;
+using Photon.Pun;
+public class TimeManeger : MonoBehaviourPunCallbacks
 {
+    [SerializeField]
+    TMPro.TextMeshProUGUI TimeText;
+    int startTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
-
-    // Update is called once per frame
-    void Update()
+	public override void OnJoinedRoom()
+	{
+	   startTime = PhotonNetwork.ServerTimestamp;
+	}
+	// Update is called once per frame
+	void Update()
     {
         
+        int time =unchecked(PhotonNetwork.ServerTimestamp-startTime);
+        string timestr = time.ToString();
+        int time_digits = timestr.Length;
+        int timeMin = time / 60000;
+        int timesec = time / 1000;
+        if (timesec >= 60)
+        {
+            timesec -= 60;
+        }
+        TimeText.text = timeMin.ToString("D2")+":"+timesec.ToString("D2")+":"+timestr.Substring(time_digits-3);
     }
 }
