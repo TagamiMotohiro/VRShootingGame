42d91d1fed204316bca2c866fe852f12be6db29a
Ryota-ika <daiouga@i.softbank.jp>
2023-02-09 10:23:35 +0900

    ryota takahashi 勝敗わかりやすくできた？？

--- UnityCS_NetWork/Assets/Scripts/GetScore.cs
+++ UnityCS_NetWork/Assets/Scripts/GetScore.cs
@@ -9,6 +9,8 @@ using UnityEngine.UI;
 public class GetScore : MainGamePUNmaneger {
     [SerializeField] GameObject score_object1=null;
     [SerializeField] GameObject score_object2=null;
+    [SerializeField] GameObject win_object;
+    [SerializeField] GameObject lose_object;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +20,26 @@ public class GetScore : MainGamePUNmaneger {
         score_text2.text = ScoreManeger.Player2score.ToString();
         //‚±‚±‚ÉŸ”s”»’è‚ð’Ç‰Á
         if (PhotonNetwork.IsMasterClient)
-        { 
-            
+        {
+            if (ScoreManeger.Player1score > ScoreManeger.Player2score)
+            {
+                win_object.SetActive(true);
+            }
+            else
+            {
+                lose_object.SetActive(true);
+            }
+        }
+        else
+        {
+            if (ScoreManeger.Player2score>ScoreManeger.Player1score)
+            {
+                win_object.SetActive(true);
+            }
+            else
+            {
+                lose_object.SetActive(true);
+            }
         }
         PhotonNetwork.Disconnect();
     }
